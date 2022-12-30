e3d75c42f76d8cb7b48c887883ebd86c64fad271
AlehKhvasko <alehkhvasko@yahoo.com>
2022-12-30 15:57:59 -0600

    init

--- Java Algorithms/DataStructureAndAlgorithms/src/main/java/com/khvasko/Main.java
+++ Java Algorithms/DataStructureAndAlgorithms/src/main/java/com/khvasko/Main.java
@@ -1,5 +1,6 @@
 package com.khvasko;
 
+import com.khvasko.doublyLinkedList.DoublyLinkedList;
 import com.khvasko.linkedlist.LinkedList;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,16 +10,21 @@ public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-        LinkedList linkedList = new LinkedList(1);
-        linkedList.append(2);
-        linkedList.append(3);
-        linkedList.append(4);
-        linkedList.printList();
-        linkedList.reverse();
-        linkedList.printList();
+//        LinkedList linkedList = new LinkedList(1);
+//        linkedList.append(2);
+//        linkedList.append(3);
+//        linkedList.append(4);
+//        linkedList.printList();
+//        linkedList.reverse();
+//        linkedList.printList();
 
+        //Doubly linkedList
+        DoublyLinkedList doublyLinkedList = new DoublyLinkedList(2);
+        doublyLinkedList.append(3);
+        doublyLinkedList.append(4);
+        System.out.println(doublyLinkedList.removeLast());
+        doublyLinkedList.printDoublyLinkedList();
     }
-
 
 
 }
--- Java Algorithms/DataStructureAndAlgorithms/src/main/java/com/khvasko/doublyLinkedList/DoublyLinkedList.java
+++ Java Algorithms/DataStructureAndAlgorithms/src/main/java/com/khvasko/doublyLinkedList/DoublyLinkedList.java
@@ -1,5 +1,7 @@
 package com.khvasko.doublyLinkedList;
 
+import com.khvasko.linkedlist.LinkedList;
+
 public class DoublyLinkedList {
     private Node head;
     private Node tail;
@@ -21,5 +23,81 @@ public class DoublyLinkedList {
         head = newNode;
         tail = newNode;
         length = 1;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void printDoublyLinkedList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            tail = newNode;
+            head = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.previos = tail;
+            tail = newNode;
+        }
+        length++;
+    }
+
+    public Node removeLast() {
+        if (length == 0) return null;
+        Node temp = tail;
+        if (length == 1) {
+            tail = null;
+            head = null;
+        } else {
+            tail = tail.previos;
+            tail.next = null;
+            temp.previos = null;
+        }
+        length--;
+        return temp;
+    }
+
+    public void prepend(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            newNode.previos = null;
+            head.previos = newNode;
+            head = newNode;
+        }
+        length++;
+    }
+
+    public Node removeFirst() {
+        if (length == 0) return null;
+        Node temp = head;
+        if (length == 1) {
+            head = null;
+            tail = null;
+        }
+        head = head.next;
+        temp.previos = null;
+        temp.next = null;
+        length--;
+        return temp;
     }
 }
