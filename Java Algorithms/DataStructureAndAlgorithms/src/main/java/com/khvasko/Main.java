fd262b3b235a259ff74e11f4940d2a932504cd8a
AlehKhvasko <alehkhvasko@yahoo.com>
2023-01-04 16:17:39 -0600

    BinarySearchTree

--- Java Algorithms/DataStructureAndAlgorithms/src/main/java/com/khvasko/Main.java
+++ Java Algorithms/DataStructureAndAlgorithms/src/main/java/com/khvasko/Main.java
@@ -2,6 +2,7 @@ package com.khvasko;
 
 import com.khvasko.doublyLinkedList.DoublyLinkedList;
 import com.khvasko.linkedlist.LinkedList;
+import com.khvasko.queue.Queue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -19,11 +20,31 @@ public class Main {
 //        linkedList.printList();
 
         //Doubly linkedList
-        DoublyLinkedList doublyLinkedList = new DoublyLinkedList(2);
-        doublyLinkedList.append(3);
-        doublyLinkedList.append(4);
-        System.out.println(doublyLinkedList.removeLast());
-        doublyLinkedList.printDoublyLinkedList();
+//        DoublyLinkedList doublyLinkedList = new DoublyLinkedList(2);
+//        doublyLinkedList.append(3);
+//        doublyLinkedList.append(4);
+//        System.out.println(doublyLinkedList.removeLast());
+//        doublyLinkedList.printDoublyLinkedList();
+
+        Queue queue = new Queue(3);
+        queue.enqueue(4);
+        queue.printQueue();
+
+        class Solution {
+            public int pivotIndex(int[] nums) {
+                int sum = 0, leftsum = 0;
+                for (int x: nums) sum += x;
+                for (int i = 0; i < nums.length; ++i) {
+                    if (leftsum == sum - leftsum - nums[i]) return i;
+                    leftsum += nums[i];
+                }
+                return -1;
+            }
+        }
+        int [] nums = {7,2,-5,2,-1,2};
+        Solution solution = new Solution();
+        System.out.println("pivot");
+        System.out.println(solution.pivotIndex(nums));
     }
 
 
