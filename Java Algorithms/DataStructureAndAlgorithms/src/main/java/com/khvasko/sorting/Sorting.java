898f258f08867203a68c0629ced09e19f20983f6
AlehKhvasko <alehkhvasko@yahoo.com>
2023-01-06 14:50:48 -0600

    Graph

---
+++ Java Algorithms/DataStructureAndAlgorithms/src/main/java/com/khvasko/sorting/Sorting.java
@@ -0,0 +1,19 @@
+package com.khvasko.sorting;
+
+public class Sorting {
+    public static void selectionSort(int [] array){
+        for (int i = 0; i < array.length ; i++) {
+            int minIndex = i;
+            for (int j = i+1; j < array.length; j++) {
+                if (array[j] < array[minIndex]){
+                    minIndex = j;
+                }
+            }
+            if (i != minIndex){
+                int temp = array[i];
+                array[i] = array[minIndex];
+                array[minIndex] = temp;
+            }
+        }
+    }
+}
